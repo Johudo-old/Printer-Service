d04d842749368043fe3b89134a83d0c236d43934
Johudo <dimazuev11@gmai.com>
2021-07-06 14:58:12 +0300

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,66 @@
+# https://medium.com/swlh/deploy-nest-js-app-with-postgres-in-vps-e1ce4abd2cad
+
+sudo apt-get update 
+sudo apt-get upgrade -y
+
+git clone https://github.com/Johudo/Printer-Service
+
+# ------------------------------
+# ------ Install Postgres ------
+# ------------------------------
+
+sudo apt-get install postgresql postgresql-contrib
+
+# TODO: COPY .env AND WRITE INFO IN 
+
+sudo -u postgres psql
+create database printer;
+create user printeruser with encrypted password 'printer307kaf';
+grant all privileges on database printer to printeruser;
+\\c printer;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+exit
+
+
+# ----------------------------
+# ------ Install NodeJS ------
+# ----------------------------
+
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
+sudo npm install pm2@latest -g
+sudo npm i -g @nestjs/cli
+
+# Build backend
+npm install
+npx typeorm migration:generate -n "InitDatabase"
+npm run build
+npx typeorm migration:run
+
+# Build frontend
+cd ./frontend
+npm install
+npm run build
+
+cd ..
+
+sudo pm2 start dist/src/main.js --name printer
+sudo pm2 startup systemd
+sudo systemctl enable pm2-root
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi --service-name printer
+sudo pm2 save
+
+
+# ---------------------------
+# ------ Install Nginx ------
+# ---------------------------
+
+sudo apt-get install nginx ufw -y
+sudo ufw allow 'Nginx Full'
+sudo ufw allow ssh
+sudo ufw enable
+
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/printer.307
+sudo ln -s /etc/nginx/sites-available/printer.307 /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl restart nginx
